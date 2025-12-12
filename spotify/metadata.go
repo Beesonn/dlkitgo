3f9775be547797a25d5f6a399525a314227cdc49
Beeson <131391958+Beesonn@users.noreply.github.com>
2025-12-12 22:32:46 +0530

    refactor: split complex functions to reduce cyclomatic complexity

--- spotify/metadata.go
+++ spotify/metadata.go
@@ -119,21 +119,7 @@ func (s *SpotifyService) GetInfo(url string) (SpotifyData, error) {
 	})
 
 	doc.Find("script[type='application/ld+json']").Each(func(i int, sel *goquery.Selection) {
-		var ldjsons []json.RawMessage
-		if err := json.Unmarshal([]byte(sel.Text()), &ldjsons); err != nil {
-			var singleLd LdJson
-			if json.Unmarshal([]byte(sel.Text()), &singleLd) == nil {
-				s.ProcessLdJson(&singleLd, &data)
-			}
-			return
-		}
-
-		for _, raw := range ldjsons {
-			var ld LdJson
-			if err := json.Unmarshal(raw, &ld); err == nil {
-				s.ProcessLdJson(&ld, &data)
-			}
-		}
+		s.ProcessLdJsonScript(sel.Text(), &data)
 	})
 
 	if (data.Type == "playlist" || data.Type == "album" || data.Type == "track") && len(data.Tracks) == 0 && data.Name == "" {
@@ -143,84 +129,138 @@ func (s *SpotifyService) GetInfo(url string) (SpotifyData, error) {
 	return data, nil
 }
 
+func (s *SpotifyService) ProcessLdJsonScript(scriptText string, data *SpotifyData) {
+	var ldjsons []json.RawMessage
+	if err := json.Unmarshal([]byte(scriptText), &ldjsons); err != nil {
+		var singleLd LdJson
+		if json.Unmarshal([]byte(scriptText), &singleLd) == nil {
+			s.ProcessLdJson(&singleLd, data)
+		}
+		return
+	}
+
+	for _, raw := range ldjsons {
+		var ld LdJson
+		if err := json.Unmarshal(raw, &ld); err == nil {
+			s.ProcessLdJson(&ld, data)
+		}
+	}
+}
+
 func (s *SpotifyService) ProcessLdJson(ld *LdJson, data *SpotifyData) {
-	if ld.Type == "MusicRecording" {
-		data.Name = ld.Name
-		if len(ld.Image) > 0 {
-			data.Image = ld.Image[0]
+	switch ld.Type {
+	case "MusicRecording":
+		s.ProcessMusicRecording(ld, data)
+	case "MusicAlbum":
+		if data.Type == "album" {
+			s.ProcessMusicAlbum(ld, data)
 		}
-		data.PreviewURL = ld.Audio.ContentURL
-		data.Artist = s.ParseArtistRaw(ld.ByArtist)
-
-		if ld.Audio.Duration != "" {
-			if dur, err := s.ParseDurationToSeconds(ld.Audio.Duration); err == nil {
-				data.Duration = dur
-			}
+	case "MusicPlaylist":
+		if data.Type == "playlist" {
+			s.ProcessMusicPlaylist(ld, data)
 		}
+	}
+}
 
-		if ld.Description != "" {
-			data.ReleaseDate = s.ExtractReleaseDate(ld.Description)
+func (s *SpotifyService) ProcessMusicRecording(ld *LdJson, data *SpotifyData) {
+	data.Name = ld.Name
+	if len(ld.Image) > 0 {
+		data.Image = ld.Image[0]
+	}
+	data.PreviewURL = ld.Audio.ContentURL
+	data.Artist = s.ParseArtistRaw(ld.ByArtist)
+
+	if ld.Audio.Duration != "" {
+		if dur, err := s.ParseDurationToSeconds(ld.Audio.Duration); err == nil {
+			data.Duration = dur
 		}
+	}
 
-		if data.Type == "track" {
-			track := TrackInfo{
-				Name:        ld.Name,
-				Artist:      data.Artist,
-				PreviewURL:  ld.Audio.ContentURL,
-				URL:         data.URL,
-				Duration:    data.Duration,
-				ReleaseDate: data.ReleaseDate,
-				Image:       data.Image,
-			}
+	if ld.Description != "" {
+		data.ReleaseDate = s.ExtractReleaseDate(ld.Description)
+	}
 
-			if ld.Audio.Duration != "" {
-				if dur, err := s.ParseDurationToSeconds(ld.Audio.Duration); err == nil {
-					track.Duration = dur
+	if data.Type == "track" {
+		s.AddTrackFromLdJson(ld, data)
+	}
+}
+
+func (s *SpotifyService) AddTrackFromLdJson(ld *LdJson, data *SpotifyData) {
+	duration := data.Duration
+	if ld.Audio.Duration != "" {
+		if dur, err := s.ParseDurationToSeconds(ld.Audio.Duration); err == nil {
+			duration = dur
+		}
+	}
+
+	track := TrackInfo{
+		Name:        ld.Name,
+		Artist:      data.Artist,
+		PreviewURL:  ld.Audio.ContentURL,
+		URL:         data.URL,
+		Duration:    duration,
+		ReleaseDate: data.ReleaseDate,
+		Image:       data.Image,
+	}
+
+	data.Tracks = append(data.Tracks, track)
+}
+
+func (s *SpotifyService) ProcessMusicAlbum(ld *LdJson, data *SpotifyData) {
+	if ld.ReleaseOf.DatePublished != "" {
+		data.ReleaseDate = ld.ReleaseOf.DatePublished
+	} else if ld.Date != "" {
+		data.ReleaseDate = ld.Date
+	}
+
+	if len(ld.Track) > 0 {
+		for _, t := range ld.Track {
+			for _, elem := range t.ItemListElement {
+				if elem.Item.Name != "" {
+					s.AddAlbumTrack(elem.Item, data)
 				}
 			}
+		}
+	}
+}
 
-			data.Tracks = append(data.Tracks, track)
+func (s *SpotifyService) AddAlbumTrack(item struct {
+	Name       string          `json:"name"`
+	PreviewURL string          `json:"previewUrl"`
+	URL        string          `json:"url"`
+	ByArtist   json.RawMessage `json:"byArtist"`
+	Audio      struct {
+		ContentURL string `json:"contentUrl"`
+		Duration   string `json:"duration,omitempty"`
+	} `json:"audio"`
+	Duration string `json:"duration,omitempty"`
+}, data *SpotifyData) {
+	var duration int
+	if item.Audio.Duration != "" {
+		if dur, err := s.ParseDurationToSeconds(item.Audio.Duration); err == nil {
+			duration = dur
 		}
-	} else if ld.Type == "MusicAlbum" && data.Type == "album" {
-		if ld.ReleaseOf.DatePublished != "" {
-			data.ReleaseDate = ld.ReleaseOf.DatePublished
-		} else if ld.Date != "" {
-			data.ReleaseDate = ld.Date
+	} else if item.Duration != "" {
+		if dur, err := s.ParseDurationToSeconds(item.Duration); err == nil {
+			duration = dur
 		}
+	}
 
-		if len(ld.Track) > 0 {
-			for _, t := range ld.Track {
-				for _, elem := range t.ItemListElement {
-					if elem.Item.Name != "" {
-						var duration int
-						if elem.Item.Audio.Duration != "" {
-							if dur, err := s.ParseDurationToSeconds(elem.Item.Audio.Duration); err == nil {
-								duration = dur
-							}
-						} else if elem.Item.Duration != "" {
-							if dur, err := s.ParseDurationToSeconds(elem.Item.Duration); err == nil {
-								duration = dur
-							}
-						}
+	data.Tracks = append(data.Tracks, TrackInfo{
+		Name:        item.Name,
+		Artist:      s.ParseArtistRaw(item.ByArtist),
+		PreviewURL:  item.Audio.ContentURL,
+		URL:         item.URL,
+		Duration:    duration,
+		ReleaseDate: data.ReleaseDate,
+		Image:       data.Image,
+	})
+}
 
-						data.Tracks = append(data.Tracks, TrackInfo{
-							Name:        elem.Item.Name,
-							Artist:      s.ParseArtistRaw(elem.Item.ByArtist),
-							PreviewURL:  elem.Item.Audio.ContentURL,
-							URL:         elem.Item.URL,
-							Duration:    duration,
-							ReleaseDate: data.ReleaseDate,
-							Image:       data.Image,
-						})
-					}
-				}
-			}
-		}
-	} else if ld.Type == "MusicPlaylist" && data.Type == "playlist" {
-		data.Name = ld.Name
-		if len(ld.Image) > 0 {
-			data.Image = ld.Image[0]
-		}
+func (s *SpotifyService) ProcessMusicPlaylist(ld *LdJson, data *SpotifyData) {
+	data.Name = ld.Name
+	if len(ld.Image) > 0 {
+		data.Image = ld.Image[0]
 	}
 }
 
@@ -266,28 +306,46 @@ func (s *SpotifyService) ParseNextData(jsonStr string, data *SpotifyData) {
 		}
 	}()
 
-	props, ok := generic["props"].(map[string]interface{})
-	if !ok {
-		return
-	}
-	pageProps, ok := props["pageProps"].(map[string]interface{})
-	if !ok {
-		return
-	}
-	state, ok := pageProps["state"].(map[string]interface{})
-	if !ok {
-		return
-	}
-	d, ok := state["data"].(map[string]interface{})
-	if !ok {
-		return
-	}
-	entity, ok := d["entity"].(map[string]interface{})
+	entity, ok := s.ExtractEntity(generic)
 	if !ok {
 		return
 	}
 
-	// Get playlist/album/track name
+	s.ExtractBasicData(entity, data)
+
+	if data.Type == "track" {
+		s.ParseSingleTrackData(entity, data)
+	} else if data.Type == "playlist" || data.Type == "album" {
+		s.ParsePlaylistOrAlbumData(entity, data)
+	}
+}
+
+func (s *SpotifyService) ExtractEntity(generic map[string]interface{}) (map[string]interface{}, bool) {
+	props, ok := generic["props"].(map[string]interface{})
+	if !ok {
+		return nil, false
+	}
+	pageProps, ok := props["pageProps"].(map[string]interface{})
+	if !ok {
+		return nil, false
+	}
+	state, ok := pageProps["state"].(map[string]interface{})
+	if !ok {
+		return nil, false
+	}
+	d, ok := state["data"].(map[string]interface{})
+	if !ok {
+		return nil, false
+	}
+	entity, ok := d["entity"].(map[string]interface{})
+	if !ok {
+		return nil, false
+	}
+
+	return entity, true
+}
+
+func (s *SpotifyService) ExtractBasicData(entity map[string]interface{}, data *SpotifyData) {
 	if data.Name == "" {
 		if title, ok := entity["name"].(string); ok {
 			data.Name = title
@@ -296,7 +354,6 @@ func (s *SpotifyService) ParseNextData(jsonStr string, data *SpotifyData) {
 		}
 	}
 
-	// Get main image
 	if data.Image == "" {
 		if images, ok := entity["images"].([]interface{}); ok && len(images) > 0 {
 			if imageMap, ok := images[0].(map[string]interface{}); ok {
@@ -306,27 +363,20 @@ func (s *SpotifyService) ParseNextData(jsonStr string, data *SpotifyData) {
 			}
 		}
 	}
-
-	// Process based on type
-	if data.Type == "track" {
-		s.parseSingleTrackData(entity, data)
-	} else if data.Type == "playlist" || data.Type == "album" {
-		s.parsePlaylistOrAlbumData(entity, data)
-	}
 }
 
-func (s *SpotifyService) parseSingleTrackData(entity map[string]interface{}, data *SpotifyData) {
-	// Get artist for single track
+func (s *SpotifyService) ParseSingleTrackData(entity map[string]interface{}, data *SpotifyData) {
+	s.ExtractTrackArtist(entity, data)
+	s.ExtractTrackPreviewURL(entity, data)
+	s.ExtractTrackDuration(entity, data)
+	s.ExtractTrackReleaseDate(entity, data)
+	s.CreateSingleTrackEntry(entity, data)
+}
+
+func (s *SpotifyService) ExtractTrackArtist(entity map[string]interface{}, data *SpotifyData) {
 	if data.Artist == "" {
 		if artists, ok := entity["artists"].([]interface{}); ok {
-			var artistNames []string
-			for _, a := range artists {
-				if artistMap, ok := a.(map[string]interface{}); ok {
-					if name, ok := artistMap["name"].(string); ok {
-						artistNames = append(artistNames, name)
-					}
-				}
-			}
+			artistNames := s.ExtractArtistNames(artists)
 			if len(artistNames) > 0 {
 				data.Artist = strings.Join(artistNames, ", ")
 			}
@@ -338,7 +388,21 @@ func (s *SpotifyService) parseSingleTrackData(entity map[string]interface{}, dat
 			data.Artist = subtitle
 		}
 	}
+}
 
+func (s *SpotifyService) ExtractArtistNames(artists []interface{}) []string {
+	var names []string
+	for _, a := range artists {
+		if artistMap, ok := a.(map[string]interface{}); ok {
+			if name, ok := artistMap["name"].(string); ok {
+				names = append(names, name)
+			}
+		}
+	}
+	return names
+}
+
+func (s *SpotifyService) ExtractTrackPreviewURL(entity map[string]interface{}, data *SpotifyData) {
 	if data.PreviewURL == "" {
 		if audioPreview, ok := entity["audioPreview"].(map[string]interface{}); ok {
 			if urlI, ok := audioPreview["url"].(string); ok {
@@ -346,11 +410,15 @@ func (s *SpotifyService) parseSingleTrackData(entity map[string]interface{}, dat
 			}
 		}
 	}
+}
 
+func (s *SpotifyService) ExtractTrackDuration(entity map[string]interface{}, data *SpotifyData) {
 	if durationMs, ok := entity["duration"].(float64); ok {
 		data.Duration = int(durationMs) / 1000
 	}
+}
 
+func (s *SpotifyService) ExtractTrackReleaseDate(entity map[string]interface{}, data *SpotifyData) {
 	if releaseDate, ok := entity["releaseDate"].(map[string]interface{}); ok {
 		if dateStr, ok := releaseDate["isoString"].(string); ok {
 			data.ReleaseDate = strings.Split(dateStr, "T")[0]
@@ -358,8 +426,9 @@ func (s *SpotifyService) parseSingleTrackData(entity map[string]interface{}, dat
 	} else if dateStr, ok := entity["releaseDate"].(string); ok {
 		data.ReleaseDate = dateStr
 	}
+}
 
-	// Create single track entry
+func (s *SpotifyService) CreateSingleTrackEntry(entity map[string]interface{}, data *SpotifyData) {
 	if data.Name != "" {
 		uri, _ := entity["uri"].(string)
 		parts := strings.Split(uri, ":")
@@ -379,14 +448,21 @@ func (s *SpotifyService) parseSingleTrackData(entity map[string]interface{}, dat
 	}
 }
 
-func (s *SpotifyService) parsePlaylistOrAlbumData(entity map[string]interface{}, data *SpotifyData) {
-	// Get artist for playlist/album (usually the owner/creator)
+func (s *SpotifyService) ParsePlaylistOrAlbumData(entity map[string]interface{}, data *SpotifyData) {
+	s.ExtractPlaylistOrAlbumArtist(entity, data)
+	s.ExtractPlaylistOrAlbumReleaseDate(entity, data)
+	s.ProcessTrackList(entity, data)
+}
+
+func (s *SpotifyService) ExtractPlaylistOrAlbumArtist(entity map[string]interface{}, data *SpotifyData) {
 	if data.Artist == "" {
 		if subtitle, ok := entity["subtitle"].(string); ok {
 			data.Artist = subtitle
 		}
 	}
+}
 
+func (s *SpotifyService) ExtractPlaylistOrAlbumReleaseDate(entity map[string]interface{}, data *SpotifyData) {
 	if releaseDate, ok := entity["releaseDate"].(map[string]interface{}); ok {
 		if dateStr, ok := releaseDate["isoString"].(string); ok {
 			data.ReleaseDate = strings.Split(dateStr, "T")[0]
@@ -394,8 +470,9 @@ func (s *SpotifyService) parsePlaylistOrAlbumData(entity map[string]interface{},
 	} else if dateStr, ok := entity["releaseDate"].(string); ok {
 		data.ReleaseDate = dateStr
 	}
+}
 
-	// Process track list for playlists and albums
+func (s *SpotifyService) ProcessTrackList(entity map[string]interface{}, data *SpotifyData) {
 	if trackList, ok := entity["trackList"].([]interface{}); ok {
 		for _, t := range trackList {
 			trackMap, ok := t.(map[string]interface{})
@@ -403,76 +480,91 @@ func (s *SpotifyService) parsePlaylistOrAlbumData(entity map[string]interface{},
 				continue
 			}
 
-			// Get track name - THIS WAS THE MAIN BUG
-			name, _ := trackMap["title"].(string)
-			if name == "" {
-				// Fallback to different field name
-				name, _ = trackMap["name"].(string)
-			}
+			s.ProcessSingleTrack(trackMap, data)
+		}
+	}
+}
 
-			// Get artist for this specific track
-			artist, _ := trackMap["subtitle"].(string)
-			if artist == "" {
-				// Try to get artists from nested structure
-				if artists, ok := trackMap["artists"].([]interface{}); ok {
-					var artistNames []string
-					for _, a := range artists {
-						if artistMap, ok := a.(map[string]interface{}); ok {
-							if artistName, ok := artistMap["name"].(string); ok {
-								artistNames = append(artistNames, artistName)
-							}
-						}
-					}
-					if len(artistNames) > 0 {
-						artist = strings.Join(artistNames, ", ")
-					}
-				}
-			}
+func (s *SpotifyService) ProcessSingleTrack(trackMap map[string]interface{}, data *SpotifyData) {
+	name := s.ExtractTrackName(trackMap)
+	if name == "" {
+		return
+	}
 
-			uri, _ := trackMap["uri"].(string)
+	artist := s.ExtractTrackArtistFromMap(trackMap)
+	uri, _ := trackMap["uri"].(string)
+	duration := s.ExtractTrackDurationFromMap(trackMap)
+	previewUrl := s.ExtractTrackPreviewURLFromMap(trackMap)
+	trackImage := s.ExtractTrackImage(trackMap, data.Image)
 
-			var duration int
-			if dur, ok := trackMap["duration"].(float64); ok {
-				duration = int(dur) / 1000
-			}
+	parts := strings.Split(uri, ":")
+	if len(parts) > 1 {
+		trackId := parts[len(parts)-1]
+		trackLink := fmt.Sprintf("https://open.spotify.com/track/%s", trackId)
+		data.Tracks = append(data.Tracks, TrackInfo{
+			Name:        name,
+			Artist:      artist,
+			PreviewURL:  previewUrl,
+			URL:         trackLink,
+			Duration:    duration,
+			ReleaseDate: data.ReleaseDate,
+			Image:       trackImage,
+		})
+	}
+}
 
-			var previewUrl string
-			if audioPreview, ok := trackMap["audioPreview"].(map[string]interface{}); ok {
-				if urlI, ok := audioPreview["url"].(string); ok {
-					previewUrl = urlI
-				}
-			}
+func (s *SpotifyService) ExtractTrackName(trackMap map[string]interface{}) string {
+	name, _ := trackMap["title"].(string)
+	if name == "" {
+		name, _ = trackMap["name"].(string)
+	}
+	return name
+}
 
-			// Get track image (album art for playlist tracks)
-			var trackImage string
-			if trackImages, ok := trackMap["images"].([]interface{}); ok && len(trackImages) > 0 {
-				if imageMap, ok := trackImages[0].(map[string]interface{}); ok {
-					if imgUrl, ok := imageMap["url"].(string); ok {
-						trackImage = imgUrl
-					}
-				}
-			}
-			// Fallback to main image if track-specific image not found
-			if trackImage == "" {
-				trackImage = data.Image
-			}
-
-			parts := strings.Split(uri, ":")
-			if len(parts) > 1 && name != "" {
-				trackId := parts[len(parts)-1]
-				trackLink := fmt.Sprintf("https://open.spotify.com/track/%s", trackId)
-				data.Tracks = append(data.Tracks, TrackInfo{
-					Name:        name, // Now correctly using the track's own name
-					Artist:      artist,
-					PreviewURL:  previewUrl,
-					URL:         trackLink,
-					Duration:    duration,
-					ReleaseDate: data.ReleaseDate,
-					Image:       trackImage,
-				})
+func (s *SpotifyService) ExtractTrackArtistFromMap(trackMap map[string]interface{}) string {
+	artist, _ := trackMap["subtitle"].(string)
+	if artist == "" {
+		if artists, ok := trackMap["artists"].([]interface{}); ok {
+			artistNames := s.ExtractArtistNames(artists)
+			if len(artistNames) > 0 {
+				artist = strings.Join(artistNames, ", ")
 			}
 		}
 	}
+	return artist
+}
+
+func (s *SpotifyService) ExtractTrackDurationFromMap(trackMap map[string]interface{}) int {
+	var duration int
+	if dur, ok := trackMap["duration"].(float64); ok {
+		duration = int(dur) / 1000
+	}
+	return duration
+}
+
+func (s *SpotifyService) ExtractTrackPreviewURLFromMap(trackMap map[string]interface{}) string {
+	var previewUrl string
+	if audioPreview, ok := trackMap["audioPreview"].(map[string]interface{}); ok {
+		if urlI, ok := audioPreview["url"].(string); ok {
+			previewUrl = urlI
+		}
+	}
+	return previewUrl
+}
+
+func (s *SpotifyService) ExtractTrackImage(trackMap map[string]interface{}, defaultImage string) string {
+	var trackImage string
+	if trackImages, ok := trackMap["images"].([]interface{}); ok && len(trackImages) > 0 {
+		if imageMap, ok := trackImages[0].(map[string]interface{}); ok {
+			if imgUrl, ok := imageMap["url"].(string); ok {
+				trackImage = imgUrl
+			}
+		}
+	}
+	if trackImage == "" {
+		trackImage = defaultImage
+	}
+	return trackImage
 }
 
 func (s *SpotifyService) ParseArtistRaw(raw json.RawMessage) string {
