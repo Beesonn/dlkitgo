69e00877c5dc661952854478d04372a2cacfab4d
Beesonn <hishammuhamed5@gmail.com>
2026-06-07 16:32:17 +0530

    Test

--- youtube/search.go
+++ youtube/search.go
@@ -50,6 +50,9 @@ func (t *TubeService) Search(query string, limit ...int) (*SearchResponse, error
 
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
 	req.Header.Set("Accept-Language", "en-US,en;q=0.9")
+   req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
+   req.Header.Set("Cookie", "CONSENT=YES+cb.20210328-17-p0.en+FX+478")
+
 
 	resp, err := t.Client.Do(req)
 	if err != nil {
